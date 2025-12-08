d9e71f7fcd65204a8702c89097bd399e678df2c0
Will Domine <willdomine1@outlook.com>
2025-12-08 14:36:12 -0600

    removing build folder

--- build/CMakeFiles/fc-tmp/glfw/download.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing download step for glfw")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/willd/Documents/Github/Aurelius/build/CMakeFiles/fc-tmp/glfw/glfw-gitclone.cmake")
-
-endblock()
--- build/CMakeFiles/fc-tmp/glfw/update.cmake
+++
@@ -1,9 +0,0 @@
-cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
-
-message(VERBOSE "Executing update step for glfw")
-
-block(SCOPE_FOR VARIABLES)
-
-include("C:/Users/willd/Documents/Github/Aurelius/build/CMakeFiles/fc-tmp/glfw/glfw-gitupdate.cmake")
-
-endblock()
